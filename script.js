1f300d8b680a58a70105229c360fada4b7c83328
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-03-23 11:42:39 +0200

    Complete TOP event handlers

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ container.appendChild(header);
 div.appendChild(divHeader);
 div.appendChild(divPara);
 container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert("Hello World");
+});
